be2edb833482c082121a03e77de28f24c089e2f8
DarFig <log.dariel@gmail.com>
2018-11-21 10:10:05 +0100

    upd match algorithm

--- python/match/match.py
+++ python/match/match.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from reader import Reader
+from tests import *
+import re
 
 r = Reader("configFile.txt")
 
@@ -8,8 +10,12 @@ a = r.openAllFiles()
 
 lines = r.getLines(0)
 
-for line in lines:
-    print(line)
 
+for i in range(1, r.getFilesNumber()):
+  file = r.getLines(i)
+  for line in lines:
+    line = re.findall('#[A-Za-z0-9]+', line)
+    if(len(line)> 0):
+      line = line[0].split('#')[1]
+      test1(line, file)
 r.closeAllFile()
-
