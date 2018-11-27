ee8ec2f9b33bfe8ca474e27aed432f65d59b0bb0
DarFig <log.dariel@gmail.com>
2018-11-27 11:58:08 +0100

    use new tests in match

--- python/match/match.py
+++ python/match/match.py
@@ -5,6 +5,10 @@ from writer import Writer
 from tests import *
 import re
 
+
+entyDicc = {}
+exDicc = {}
+
 r = Reader("configFile.txt")
 w = Writer()
 w.openAllFiles()
@@ -18,11 +22,18 @@ lines = r.getLines(0)
 for i in range(1, r.getFilesNumber()):
   file = r.getLines(i)
   for line in lines:
+    cad = getTablasYPropiedades(line)
+    if(cad is not ""):
+      for c in cad:
+        if (c not in entyDicc): 
+          entyDicc[c] = c
+          w.setLine(1, c+"\n\r")
     line = re.findall('#[A-Za-z0-9]+', line)
     if(len(line)> 0):
       line = line[0].split('#')[1]
-      test1(line, file, w)
-
+      test1(line, file, w, exDicc)
+      
+testFinal(entyDicc, exDicc, w)
 
 
 r.closeAllFile()
