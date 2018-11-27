9c98d7c9d3fbbf471fc85023facd159fbeee915f
DarFig <log.dariel@gmail.com>
2018-11-27 12:16:32 +0100

    fix/add main to match xD

--- python/match/match.py
+++ python/match/match.py
@@ -6,35 +6,41 @@ from tests import *
 import re
 
 
-entyDicc = {}
-exDicc = {}
-
-r = Reader("configFile.txt")
-w = Writer()
-w.openAllFiles()
 
 
-a = r.openAllFiles()
+def main():
+  entyDicc = {}
+  exDicc = {}
 
-lines = r.getLines(0)
-
-
-for i in range(1, r.getFilesNumber()):
-  file = r.getLines(i)
-  for line in lines:
-    cad = getTablasYPropiedades(line)
-    if(cad is not ""):
-      for c in cad:
-        if (c not in entyDicc): 
-          entyDicc[c] = c
-          w.setLine(1, c+"\n\r")
-    line = re.findall('#[A-Za-z0-9]+', line)
-    if(len(line)> 0):
-      line = line[0].split('#')[1]
-      test1(line, file, w, exDicc)
-      
-testFinal(entyDicc, exDicc, w)
+  r = Reader("configFile.txt")
+  w = Writer()
+  w.openAllFiles()
 
 
-r.closeAllFile()
-w.closeAllFile()
+  a = r.openAllFiles()
+
+  lines = r.getLines(0)
+
+
+  for i in range(1, r.getFilesNumber()):
+    file = r.getLines(i)
+    for line in lines:
+      cad = getTablasYPropiedades(line)
+      if(cad is not ""):
+        for c in cad:
+          if (c not in entyDicc): 
+            entyDicc[c] = c
+            w.setLine(1, c+"\n\r")
+      line = re.findall('#[A-Za-z0-9]+', line)
+      if(len(line)> 0):
+        line = line[0].split('#')[1]
+        test1(line, file, w, exDicc)
+        
+  testFinal(entyDicc, exDicc, w)
+
+
+  r.closeAllFile()
+  w.closeAllFile()
+
+
+main()
