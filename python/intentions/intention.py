0b30ebabcaa21fa2b9f2a03e2764eda4418f5b1a
darfig <log.dariel@gmail.com>
2019-01-17 14:23:17 +0100

    upd

--- python/intentions/intention.py
+++ python/intentions/intention.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 class EngineClass():
- 
+    
+
     def __init__(self, text = "", tokenList=[]):
         self.__text = text
         self.__tokenList = tokenList
+        self.__markers = [". ", "." "-"]
 
     def settext(self, text):
         self.__text = text
@@ -14,19 +16,26 @@ class EngineClass():
     def run(self, text):
         self.__text = text
         if self.__inText(self.__tokenList[0]) and self.__inText(self.__tokenList[1])  and self.__inText(self.__tokenList[2]):
-            return ""
+            return self.__text
         for i in range(1, len(self.__tokenList)):
             if self.__inText(self.__tokenList[0]) and self.__inText(self.__tokenList[i]):
-                return ""
+                return  self.__retText(self.__text)
             
 
     # private
     def __inText(self, token):
-        return token in self.__text
+        return self.__clearToken(token) in self.__text
 
-    def __getEntities(self, token1, token2):
-        return self.__text.split(token1)[1].split(token2)[0]
+    def __getEntity(self, numb1, numb2):
+        return self.__text.split(self.__tokenList[numb1])[1].split(self.__tokenList[numb2])[0]
 
-    
+    def __clearToken(self, token):
+        for mark in self.__markers:
+            token = token.replace(mark, "")
+        return token
+
+    def __retText(self, index):
+        ret = self.__tokenList[0] + self.__getEntity(0,index)
+        return ret
 
 
--- python/intentions/main.py
+++ python/intentions/main.py
@@ -3,6 +3,6 @@
 from intention import EngineClass
 
 if __name__== "__main__":
-    tokens = ["convocatoria", "lugar", "fecha"]
+    tokens = ["convocatoria", ". lugar", "fecha."]
     engine = EngineClass("convocatoria uno lugar dos fecha tres", tokens)
     print(engine.run("convocatoria uno lugar dos fecha tres"))
