1ac89c1dc9606836e9ac671afcb3b2eca9ee8c39
darfig <log.dariel@gmail.com>
2019-01-18 10:54:17 +0100

    add getEntities

--- python/intentions/intention.py
+++ python/intentions/intention.py
@@ -13,14 +13,34 @@ class EngineClass():
     def gettext(self, text):
         return self.__text
 
-    def run(self, text):
-        self.__text = text
+    def run(self, text=""):
+        if text is not "":
+            self.__text = text
         for i in range(2, len(self.__tokenList)):
             if self.__inText(self.__tokenList[0]) and self.__inText(self.__tokenList[1])  and self.__inText(self.__tokenList[i]):
                 return self.__text
         for i in range(1, len(self.__tokenList)):
             if self.__inText(self.__tokenList[0]) and self.__inText(self.__tokenList[i]):
                 return  self.__retText(0,i) + self.__retText(i,i+1)
+
+    def getEntities(self, text=""):
+        if text is not "":
+            self.__text = text
+        listEntities = []
+        i = 0
+        encontrado = False
+        for token in self.__tokenList:         
+            if self.__inText(token):
+                for j in range(i+1, len( self.__tokenList)):         
+                    if self.__inText(self.__tokenList[j]):
+                        encontrado = True
+                        listEntities.append(self.__getEntity(i, j))
+                        i = j
+                        break
+                if not encontrado:
+                    listEntities.append(self.__getEntity(i, i+1))
+                encontrado = False
+        return listEntities
             
 
     # private
--- python/intentions/main.py
+++ python/intentions/main.py
@@ -5,8 +5,8 @@ from intention import EngineClass
 if __name__== "__main__":
     tokens = ["convocatoria", ". lugar", "fecha .", "tipo_pub ."]
     engine = EngineClass("convocatoria uno lugar dos fecha tres", tokens)
-    print(engine.run("convocatoria uno fecha tres"))
-    print(engine.run("convocatoria uno lugar dos"))
-    print(engine.run("convocatoria uno tipo_pub tres "))
-    print(engine.run("convocatoria uno lugar dos fecha tres"))
-    print(engine.run("convocatoria uno lugar dos tipo_pub tres"))
+    print(engine.run("convocatoria uno fecha tres")," + " ,engine.getEntities())
+    print(engine.run("convocatoria uno lugar dos")," + " ,engine.getEntities())
+    print(engine.run("convocatoria uno tipo_pub tres ")," + " ,engine.getEntities())
+    print(engine.run("convocatoria uno lugar dos fecha tres")," + " ,engine.getEntities())
+    print(engine.run("convocatoria uno lugar dos tipo_pub tres")," + " ,engine.getEntities())       
