99349d7a14f25760efdcc110ce0de3ac16fb1f5e
darfig <log.dariel@gmail.com>
2019-01-18 11:54:33 +0100

    fix main

--- python/intentions/main.py
+++ python/intentions/main.py
@@ -4,7 +4,7 @@ from intention import EntitiesEngine
 
 if __name__== "__main__":
     tokens = ["convocatoria", ". lugar", "fecha .", "tipo_pub ."]
-    engine = LemmaClass("convocatoria uno lugar dos fecha tres", tokens)
+    engine = EntitiesEngine("convocatoria uno lugar dos fecha tres", tokens)
     print(engine.run("convocatoria uno fecha tres")," + " ,engine.getEntities())
     print(engine.run("convocatoria uno lugar dos")," + " ,engine.getEntities())
     print(engine.run("convocatoria uno tipo_pub tres ")," + " ,engine.getEntities())
