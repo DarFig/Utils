6ad7d47293d76f41f119e0a1dbcaf3ff07b76c10
darfig <log.dariel@gmail.com>
2019-01-21 08:19:51 +0100

    modificados ejemplos

--- python/intentions/main.py
+++ python/intentions/main.py
@@ -3,11 +3,11 @@
 from intention import EntitiesEngine
 
 if __name__== "__main__":
-    tokens = ["convocatoria", ". lugar", "fecha .", "tipo_pub ."]
+    tokens = ["convocatoria", ". lugar", "tipo_pub .", "fecha ."]
     engine = EntitiesEngine("convocatoria uno lugar dos fecha tres", tokens)
     print(engine.run("convocatoria uno fecha tres")," + " ,engine.getEntities())
     print(engine.run("convocatoria uno lugar dos")," + " ,engine.getEntities())
     print(engine.run("convocatoria uno tipo_pub tres")," + " ,engine.getEntities())
-    print(engine.run("convocatoria uno fecha dos tipo_pub tres")," + " ,engine.getEntities())
+    print(engine.run("convocatoria uno tipo_pub dos fecha tres ")," + " ,engine.getEntities())
     print(engine.run("convocatoria uno lugar dos fecha tres")," + " ,engine.getEntities())
     print(engine.run("convocatoria uno lugar dos tipo_pub tres")," + " ,engine.getEntities())       
