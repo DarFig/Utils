a4fc3946f35a82342c89e9594d64fd9c36348bdf
darfig <log.dariel@gmail.com>
2019-09-02 12:39:24 +0200

    upd test rust/stack

--- rust/stack.rs
+++ rust/stack.rs
@@ -18,7 +18,7 @@ impl<T> Stack<T> {
     }
 
     pub fn push(&mut self, element: T) {
-        let node = Box:new(Node {
+        let node = Box::new(Node {
         element: element,
         next: self.top.take(),
         }); 
@@ -26,7 +26,7 @@ impl<T> Stack<T> {
     }
 
     pub fn pop(&mut self) -> Option<T> {
-        self.top.take().map(|Node| {
+        self.top.take().map(|node| {
             self.top = node.next;
             node.element
         })
@@ -38,9 +38,33 @@ impl<T> Drop for Stack<T> {
     fn drop(&mut self) {
         let mut actual = self.top.take();
         while let Some(mut link) = actual {
-            actual = link.next.take()
+            actual = link.next.take();
         }
     }
 }
 
+#[cfg(test)]
+mod test {
+    use super::Stack;
+    #[test]
+    fn empty_test(){
+        let mut stack:Stack<i32> = Stack::new();
+        assert_eq!(stack.pop(), None);
+    }
 
+    #[test]
+    fn push_pop_test(){
+        let mut stack = Stack::new();
+        
+        stack.push(22);
+        stack.push(23);
+        assert_eq!(stack.pop(), Some(23));
+        assert_eq!(stack.pop(), Some(22));
+        stack.push(2);
+        stack.push(3);
+        assert_eq!(stack.pop(), Some(3));
+        assert_eq!(stack.pop(), Some(2));
+        assert_eq!(stack.pop(), None);
+    }
+
+}
